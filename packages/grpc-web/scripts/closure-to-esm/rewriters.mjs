03ea5ffc65f3d81dfe8dc4414b7eb46e418d71b4
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-10-17 13:39:06 +0200

    Make tests for multiple exports pass

--- packages/grpc-web/scripts/closure-to-esm/rewriters.mjs
+++ packages/grpc-web/scripts/closure-to-esm/rewriters.mjs
@@ -5,48 +5,39 @@ export const REWRITERS = [rewriteRequires, rewriteModules, rewriteProvides];
 // @rewrite function
 export function rewriteProvides(filestr) {
   return filestr.replace(
-    /(\S*goog.provide\('([.a-zA-Z]+)'\);?\s)+/,
-    (match) => {
-      const lines = match.split("\n").filter((it) => it !== "");
+    /^([ \t]*goog.provide\('([.a-zA-Z]+)'\));?$/gm,
+    (it) => {
+      const matches = it.match(REGEX_PATH);
+      const requireName = matches.pop();
+      const exportName = requireName?.split(".").pop() ?? "undefined";
 
-      const out = `export {
-    ${lines
-      .map((line) => {
-        const matches = line.match(REGEX_PATH);
-        const provideName = matches.pop();
-
-        return provideName.split(".").pop();
-      })
-      .join(",\n  ")}
-  }`;
-
-      return out;
+      return `export { ${exportName} };`;
     }
   );
 }
 
 // @rewrite function
 export function rewriteModules(filestr) {
-  return filestr.replace(/^goog.module\('([.a-zA-Z]+)'\);?$/m, (it) => {
+  return filestr.replace(/^[ \t]*goog.module\('([.a-zA-Z]+)'\);?$/m, (it) => {
     const matches = it.match(REGEX_PATH);
     const requireName = matches.pop();
 
     const symbolName = requireName?.split(".").pop() ?? "undefined";
-    return `export { ${symbolName} }`;
+    return `export { ${symbolName} };`;
   });
 }
 
 // @rewrite function
 export function rewriteRequires(filestr) {
   return filestr.replace(
-    /^((const|var)\s+([a-zA-Z]+)\s+=\s+)?goog.require(Type)?\('([.a-zA-Z]+)'\)/gm,
+    /^[ \t]*((const|var)[ \t]+([a-zA-Z]+)[ \t]+=[ \t]+)?goog.require(Type)?\('([.a-zA-Z]+)'\);?/gm,
     (it) => {
       const matches = it.match(REGEX_PATH);
       const requireName = matches.pop();
 
-      if (it.startsWith("goog.require")) {
+      if (it.trim().startsWith("goog.require")) {
         const symbolName = requireName?.split(".").pop() ?? "undefined";
-        return `import { ${symbolName} } from "./${requireName}.js"`;
+        return `import { ${symbolName} } from "./${requireName}.js";`;
       } else {
         return it;
       }
--- packages/grpc-web/scripts/closure-to-esm/rewriters.test.mjs
+++ packages/grpc-web/scripts/closure-to-esm/rewriters.test.mjs
@@ -5,11 +5,13 @@ import {
 } from "./rewriters.mjs";
 
 testRewriter({
-  title:
-    "Given goog.provide('goog.Example') it should return export { Example }",
+  title: `Given a single "goog.provide" it should return a single "export"`,
   rewriter: rewriteProvides,
   cases: [
-    { input: `goog.provide('goog.Example');`, output: `export { Example };` },
+    {
+      input: `goog.provide('goog.Example');`,
+      output: `export { Example };`,
+    },
     {
       input: `
 
@@ -24,14 +26,97 @@ export { Example };
   ],
 });
 
+testRewriter({
+  title: `Given multiple "goog.provide" it should return a multiple "export"`,
+  rewriter: rewriteProvides,
+  cases: [
+    {
+      input: `
+  goog.provide('goog.Example');
+goog.provide('goog.Example.Two');
+
+class Example;
+`,
+      output: `
+export { Example };
+export { Two };
+
+class Example;
+`,
+    },
+    {
+      input: `
+
+      goog.provide('goog.Example');
+      goog.provide('goog.Example.Two');
+goog.provide('goog.haa.ha.ha.Example.Four');
+goog.provide('goog.Example.Ten');
+`,
+
+      output: `
+
+export { Example };
+export { Two };
+export { Four };
+export { Ten };
+`,
+    },
+  ],
+});
+
+testRewriter({
+  title: `Given "goog.module" it should return "export"`,
+  rewriter: rewriteModules,
+  cases: [
+    {
+      input: `goog.module('goog.Example');`,
+      output: `export { Example };`,
+    },
+    {
+      input: `
+  
+  goog.module('goog.my.Example');
+  `,
+
+      output: `
+  
+export { Example };
+  `,
+    },
+  ],
+});
+
+testRewriter({
+  title: `Given "goog.require" it should return "import-from"`,
+  rewriter: rewriteRequires,
+  cases: [
+    {
+      input: `goog.require('goog.Example');`,
+      output: `import { Example } from "./goog.Example.js";`,
+    },
+    {
+      input: `
+    
+    goog.require('goog.my.Example');
+    `,
+
+      output: `
+    
+import { Example } from "./goog.my.Example.js";
+    `,
+    },
+  ],
+});
+
+// testRewriter basically defines a micro testing framework specialized only for testing rewriters.mjs
 function testRewriter({ title, rewriter, cases }) {
   for (const { input, output } of cases) {
     const actual = rewriter(input);
-    if (actual.trim() === output.trim()) {
+    if (actual === output) {
       console.log(`[rewriters.test.mjs] ${title}:`);
     } else {
-      console.log(`[rewriters.test.mjs] ${title}\nFAILURE!\n:`);
-      console.log({ expect: output, actual });
+      console.log(`[rewriters.test.mjs] ${title}\nFAILURE!:`);
+      console.log({ given: input, expect: output, actual });
     }
   }
 }
