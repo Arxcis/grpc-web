6a3a0423f7b11b04c98fd25d54fa16e23559a775
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-10-18 18:59:23 +0200

    Wrap sed-string in execShellCommand function-call

--- packages/grpc-web/scripts/closuretoesm/execshellcommand.js
+++ packages/grpc-web/scripts/closuretoesm/execshellcommand.js
@@ -1,19 +1,21 @@
 import { exec } from "child_process";
 
 export async function appendLineToFile(line, filepath) {
-  await execShellCommand(`echo "${line}" >> "${filepath}"`);
+  return await execShellCommand(`echo "${line}" >> "${filepath}"`);
 }
 
 export async function replaceLine(before, after, path) {
-  await execShellCommand(`sed -i "s/${before}/${after}/g" "${path}"`);
+  return await execShellCommand(`sed -i "s/${before}/${after}/g" "${path}"`);
 }
 
 export async function deleteLine(line, path) {
-  `sed -i "/${line}/d" "${path}"`;
+  return await execShellCommand(`sed -i "/${line}/d" "${path}"`);
 }
 
 export async function appendLineToLine(lineToAppendTo, newLine, path) {
-  return `sed -i "/${lineToAppendTo}/a ${newLine}" "${path}"`;
+  return await execShellCommand(
+    `sed -i "/${lineToAppendTo}/a ${newLine}" "${path}"`
+  );
 }
 
 export function execShellCommand(cmd) {
