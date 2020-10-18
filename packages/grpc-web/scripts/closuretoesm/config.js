1bcfcaca8c6cb0b4e580785c33409f3200e0365d
Jonas Johan Solsvik <jonasjso@protonmail.com>
2020-10-18 12:45:11 +0200

    Rename esm_modules/ -> es_modules/

--- packages/grpc-web/scripts/closuretoesm/config.js
+++ packages/grpc-web/scripts/closuretoesm/config.js
@@ -1,4 +1,4 @@
 export const ENTRYPOINT = "./exports.js";
-export const OUT_DIR = "./esm_modules";
+export const OUT_DIR = "./es_modules";
 export const GOOG_DIR = "../../third_party/closure-library/closure/goog/";
 export const INCLUDE_DIRS = ["../../javascript/net/grpc/web", GOOG_DIR];
